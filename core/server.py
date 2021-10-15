1ff7b10f7895841a0c836d843e3efbd239198057
le3ch-tech <sam.moes15@gmail.com>
2021-10-15 22:47:18 +0200

    push

--- core/server.py
+++ core/server.py
@@ -8,10 +8,33 @@ from src.colors import BColors
 from src.dependencies import *
 
 command = lambda x: os.system(x)
+refresh = lambda x: os.system('python3 core/' + x)
 command('clear')
 
-if __name__ == '__main__':
-    util.print_banner('HERMOD')
+def menue(title):
+    util.print_banner(title)
     print(BColors.RED + '\n                              BOTNET')
     print(BColors.WHITE + '\n                        Written by ' + BColors.RED + 'DAEM0N\n')
     util.print_spacer()
+
+if __name__ == '__main__':
+    menue('HERMOD')
+    
+    while True:
+
+        try:
+            input = input(BColors.RED + ">>" + BColors.WHITE + " ")
+
+            if input == 'ping':
+                print('pinged')
+                time.sleep(0.5)
+            elif input == '2':
+                print('2')
+            else:
+                print('Invalid Syntax!')
+                time.sleep(0.5)
+                refresh('server.py')
+
+        except KeyboardInterrupt:
+            print("\nProgram closed by user (CTRL+C)\n")
+            exit()
--- hermod.py
+++ hermod.py
@@ -1,14 +1,44 @@
 import os
+import json
 
-print('-> 1) Install Dependecies')
-print('~~~~~~~~~~~~~~~~~~~~~~~~~')
-print('-> 2) Launch Bot         ')
+command = lambda x: os.system(x)
+command('clear')
 
-choice = input('Please select: ->  [1, 2, ...] : ')
-if choice == '1':
-    os.system('python3 core/src/dependencies.py')
-elif choice == '2':
-    os.system('python3 core/server.py')
-else:
-    print(f'Invalid Input: {choice}')
-    os.system('python3 hermod.py')
+def check_config():
+    try:
+        config_file = open('core/config/config.json')
+        config = json.load(config_file)
+
+        if config['quickstart']:
+            os.system('python3 core/server.py')
+        elif not config['quickstart']:
+            return print('CONFIG: Quickstart: -> false\n')
+    except FileNotFoundError:
+        print('CONFIG: Not found, Installed Correctly?')
+        exit()
+    except json.decoder.JSONDecodeError as err:
+        print("Unable to read JSON File:\n" + format(err))
+        exit()
+    except KeyError:
+        pass
+
+if __name__ == "__main__":
+
+    try:
+        check_config()
+
+        print('1) Install Dependecies\n')
+        print('2) Launch Server      \n')
+
+        choice = input('Please select: ->  [1, 2, ...] : ')
+        if choice == '1':
+            os.system('python3 core/src/dependencies.py')
+        elif choice == '2':
+            os.system('python3 core/server.py')
+        else:
+            print(f'Invalid Input: {choice}')
+            os.system('python3 hermod.py')
+    
+    except KeyboardInterrupt:
+        print("\nProgram closed by user (CTRL+C)\n")
+        exit()
